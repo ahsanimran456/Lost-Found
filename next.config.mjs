78ffdcef50c5f0080e7aa62f42a04d54be033018
ahsan <m.ahsan.symits@gmail.com>
2024-11-23 15:47:20 +0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import withPWA from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+export default withPWA({
+  dest: 'public',       // Service worker aur caching files ko "public" folder mein save karta hai
+  register: true,       // Automatically service worker ko register karta hai
+  skipWaiting: true,    // Naye updates ke liye purani service worker ko wait nahi karata
+  ...nextConfig,        // Extra Next.js configuration
+});
